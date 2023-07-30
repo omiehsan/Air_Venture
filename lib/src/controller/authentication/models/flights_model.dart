f71e035ae7f12d518534836d256cd122167453c9
Tashrif-alive <111632512+Tashrif-alive@users.noreply.github.com>
2023-07-30 21:43:08 +0600

    update

--- lib/src/controller/authentication/models/flights_model.dart
+++ lib/src/controller/authentication/models/flights_model.dart
@@ -1,10 +1,73 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+//
+// class FlightSearchModel {
+//   String? id;
+//   final String? duration;
+//   final String? airline;
+//   final num? price;
+//   final String fromDestination;
+//   final String toDestination;
+//   final String flightClass;
+//   final DateTime date;
+//
+//   FlightSearchModel({
+//     this.id,
+//     required this.fromDestination,
+//     required this.toDestination,
+//     required this.date,
+//     required this.flightClass,
+//     required this.duration,
+//     required this.price,
+//     required this.airline,
+//   });
+//
+//   toJson() {
+//     return {
+//       "From": fromDestination,
+//       "To": toDestination,
+//       "Class": flightClass,
+//       "Duration": duration,
+//       "Price": price,
+//       "Airline": airline,
+//       "Date": date,
+//     };
+//   }
+//
+//   factory FlightSearchModel.fromDatabase(
+//       DocumentSnapshot<Map<String, dynamic>> document) {
+//     final flightData = document.data()!;
+//
+//     parseDate(data) {
+//       if (data == null || data == "") {
+//         return DateTime.parse("2023-03-12T18:42:49.608466Z");
+//       } else {
+//         final timeToMS = data.millisecondsSinceEpoch;
+//         return DateTime.fromMillisecondsSinceEpoch(timeToMS);
+//       }
+//     }
+//
+//     return FlightSearchModel(
+//       id: document.id,
+//       fromDestination: flightData["From"],
+//       toDestination: flightData["To"],
+//       date: parseDate(flightData["Date"]),
+//       flightClass: flightData["Class"],
+//       duration: flightData["Duration"],
+//       price: flightData["Price"],
+//       airline: flightData["Airline"],
+//     );
+//   }
+// }
+
+
+import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FlightSearchModel {
   String? id;
-  final String? duration;
-  final String? airline;
-  final num? price;
+  String? duration;
+  String? airline;
+  num? price;
   final String fromDestination;
   final String toDestination;
   final String flightClass;
@@ -16,10 +79,25 @@ class FlightSearchModel {
     required this.toDestination,
     required this.date,
     required this.flightClass,
-    required this.duration,
-    required this.price,
-    required this.airline,
+    this.duration,
+    this.price,
+    this.airline,
   });
+
+  // Factory method to create a FlightSearchModel from a Firestore DocumentSnapshot
+  factory FlightSearchModel.fromSnapshot(DocumentSnapshot snapshot) {
+    final data = snapshot.data() as Map<String, dynamic>;
+    return FlightSearchModel(
+      id: snapshot.id,
+      fromDestination: data['From'],
+      toDestination: data['To'],
+      flightClass: data['Class'],
+      duration: data['Duration'],
+      price: data['Price'],
+      airline: data['Airline'],
+      date: (data['Date'] as Timestamp).toDate(),
+    );
+  }
 
   toJson() {
     return {
@@ -32,29 +110,35 @@ class FlightSearchModel {
       "Date": date,
     };
   }
+}
 
-  factory FlightSearchModel.fromDatabase(
-      DocumentSnapshot<Map<String, dynamic>> document) {
-    final flightData = document.data()!;
+// Function to search flights based on given parameters
+Future<List<FlightSearchModel>> searchFlights(
+    String fromDestination,
+    String toDestination,
+    String flightClass,
+    DateTime date,
+    ) async {
+  final firestore = FirebaseFirestore.instance;
+  final flightCollection = firestore.collection('flights');
 
-    parseDate(data) {
-      if (data == null || data == "") {
-        return DateTime.parse("2023-03-12T18:42:49.608466Z");
-      } else {
-        final timeToMS = data.millisecondsSinceEpoch;
-        return DateTime.fromMillisecondsSinceEpoch(timeToMS);
-      }
-    }
+  try {
+    // Perform the search query using where clauses to filter the data
+    final query = await flightCollection
+        .where('From', isEqualTo: fromDestination)
+        .where('To', isEqualTo: toDestination)
+        .where('Class', isEqualTo: flightClass)
+        .where('Date', isEqualTo: date)
+        .get();
 
-    return FlightSearchModel(
-      id: document.id,
-      fromDestination: flightData["From"],
-      toDestination: flightData["To"],
-      date: parseDate(flightData["Date"]),
-      flightClass: flightData["Class"],
-      duration: flightData["Duration"],
-      price: flightData["Price"],
-      airline: flightData["Airline"],
-    );
+    // Convert the query results to FlightSearchModel objects
+    final List<FlightSearchModel> flightResults = query.docs
+        .map((doc) => FlightSearchModel.fromSnapshot(doc))
+        .toList();
+
+    return flightResults;
+  } catch (e) {
+    print('Error while searching flights: $e');
+    return [];
   }
 }
--- lib/src/view/navigations/search_bar/search_boxs.dart
+++ lib/src/view/navigations/search_bar/search_boxs.dart
@@ -1,6 +1,239 @@
+// import 'package:fluentui_icons/fluentui_icons.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hawai_jubu/src/view/navigations/search_bar/dropdown.dart';
+// import '../../../utils/constaints/styles_colors.dart';
+//
+//
+// class SearchBoxs extends StatefulWidget {
+//   const SearchBoxs({Key? key}) : super(key: key);
+//
+//   @override
+//   _SearchBoxsState createState() => _SearchBoxsState();
+// }
+//
+// class _SearchBoxsState extends State<SearchBoxs> {
+//   bool isClicked = false;
+//
+//   void onTap() {
+//     setState(() {
+//       isClicked = !isClicked;
+//     });
+//   }
+//
+//   DateTime _dateTime = DateTime.now();
+//
+//   void _showDatePicker() {
+//     showDatePicker(
+//       context: context,
+//       initialDate: DateTime.now(),
+//       firstDate: DateTime.now(),
+//       lastDate: DateTime(2025),
+//     ).then((value) {
+//       setState(() {
+//         _dateTime = value!;
+//       });
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       color: Colors.brown.shade100,
+//       padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10),
+//       child: ListView(
+//         shrinkWrap: true,
+//         physics: const NeverScrollableScrollPhysics(),
+//         children: [
+//           Container(
+//             padding: const EdgeInsets.symmetric(horizontal: 15),
+//             child: Row(
+//               children: [
+//                 ElevatedButton(
+//                   onPressed: onTap,
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor:
+//                         isClicked ? Colors.white70 : Colors.black12,
+//                     shape: RoundedRectangleBorder(
+//                         borderRadius: BorderRadius.circular(10)),
+//                   ),
+//                   child: const Text(
+//                     ' One Way ',
+//                     style: TextStyle(
+//                       fontSize: 16,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.white,
+//                     ),
+//                   ),
+//                 ),
+//                 const SizedBox(
+//                   width: 8,
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: onTap,
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor:
+//                         isClicked ? Colors.black12 : Colors.white70,
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(10),
+//                     ),
+//                   ),
+//                   child: const Text(
+//                     'Round Trip',
+//                     style: TextStyle(
+//                       fontSize: 16,
+//                       fontWeight: FontWeight.bold,
+//                       color: Colors.black,
+//                     ),
+//                   ),
+//                 ),
+//               ],
+//             ),
+//           ),
+//           const SizedBox(height: 5),
+//           ListTile(
+//             title: SizedBox(
+//               height: MediaQuery.of(context).size.height * 0.06,
+//               child: TextFormField(
+//                 decoration: InputDecoration(
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(5),
+//                       borderSide: BorderSide.none,
+//                     ),
+//                     fillColor: Colors.black26,
+//                     filled: true,
+//                     prefixIcon: const Icon(
+//                       Icons.location_on,
+//                       color: Colors.white70,
+//                     ),
+//                     hintText: 'From',
+//                     hintStyle: const TextStyle(
+//                       color: Colors.white,
+//                     ),
+//                     contentPadding:
+//                         EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+//               ),
+//             ),
+//           ),
+//           ListTile(
+//             title: SizedBox(
+//               height: MediaQuery.of(context).size.height * 0.06,
+//               child: TextFormField(
+//                 decoration: InputDecoration(
+//                     border: OutlineInputBorder(
+//                       borderRadius: BorderRadius.circular(5),
+//                       borderSide: BorderSide.none,
+//                     ),
+//                     fillColor: Colors.black26,
+//                     filled: true,
+//                     prefixIcon: const Icon(
+//                       FluentSystemIcons.ic_fluent_globe_location_regular,
+//                       color: Colors.white70,
+//                     ),
+//                     hintText: 'To',
+//                     hintStyle: TextStyle(
+//                       color: Colors.white,
+//                     ),
+//                     contentPadding:
+//                         EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+//               ),
+//             ),
+//           ),
+//           ListTile(
+//             title: SizedBox(
+//               height: MediaQuery.of(context).size.height * 0.06,
+//               child: InkWell(
+//                 onTap: _showDatePicker,
+//                 child: Container(
+//                   decoration: BoxDecoration(
+//                     borderRadius: BorderRadius.circular(5),
+//                     color: Colors.black26,
+//                   ),
+//                   padding:
+//                       const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+//                   child: Row(
+//                     children: [
+//                       const Padding(
+//                         padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+//                         child:
+//                             Icon(Icons.calendar_month, color: Colors.white70),
+//                       ),
+//                       Text(_dateTime.toString(), style: Styles.headLineStyle6)
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//           // ListTile(
+//           //   title: SizedBox(
+//           //     height: MediaQuery.of(context).size.height * 0.06,
+//           //     child: InkWell(
+//           //       onTap: () {},
+//           //       child: Container(
+//           //         decoration: BoxDecoration(
+//           //           borderRadius: BorderRadius.circular(5),
+//           //           color: Colors.black26,
+//           //         ),
+//           //         padding: const EdgeInsets.symmetric(
+//           //           horizontal: 12,
+//           //           vertical: 10,
+//           //         ),
+//           //         child: Row(
+//           //           children: const [
+//           //             Padding(
+//           //               padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+//           //               child: Icon(
+//           //                 Icons.supervisor_account,
+//           //                 color: Colors.white70,
+//           //               ),
+//           //             ),
+//           //             Text(
+//           //               'Traveller',
+//           //               style: TextStyle(
+//           //                 color: Colors.white,
+//           //               ),
+//           //             ),
+//           //           ],
+//           //         ),
+//           //       ),
+//           //     ),
+//           //   ),
+//           // ),
+//           MyListTileWithDropdown(),
+//           const SizedBox(height: 5),
+//           Center(
+//             child: Container(
+//               width: MediaQuery.of(context).size.width * 0.9,
+//               height: 44.0,
+//               decoration: BoxDecoration(
+//                 borderRadius: BorderRadius.circular(10),
+//                 gradient: const LinearGradient(
+//                   colors: [Color(0xFFff9133), Color(0xFFe16c06)],
+//                 ),
+//               ),
+//               child: ElevatedButton(
+//                 onPressed: () {},
+//                 style: ElevatedButton.styleFrom(
+//                   backgroundColor: Colors.transparent,
+//                   shape: RoundedRectangleBorder(
+//                     borderRadius: BorderRadius.circular(10),
+//                   ),
+//                 ),
+//                 child: const Text('Search'),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:hawai_jubu/src/view/navigations/search_bar/dropdown.dart';
 import '../../../utils/constaints/styles_colors.dart';
 
 
@@ -35,6 +268,33 @@ class _SearchBoxsState extends State<SearchBoxs> {
     });
   }
 
+  void onSearchPressed() async {
+    String fromDestination = ""; // Get the value from the From text field
+    String toDestination = ""; // Get the value from the To text field
+    DateTime date = _dateTime;
+
+    try {
+      final querySnapshot = await FirebaseFirestore.instance
+          .collection('flights')
+          .where('From', isEqualTo: fromDestination)
+          .where('To', isEqualTo: toDestination)
+          .where('Date', isEqualTo: date)
+          .get();
+
+      // Process the query results
+
+
+      final List<DocumentSnapshot> flightDocs = querySnapshot.docs;
+
+
+      // You can now use the data from flightDocs to display the search results
+
+    } catch (e) {
+      print('Error while searching flights: $e');
+      // Handle the error if needed
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -52,7 +312,7 @@ class _SearchBoxsState extends State<SearchBoxs> {
                   onPressed: onTap,
                   style: ElevatedButton.styleFrom(
                     backgroundColor:
-                        isClicked ? Colors.white70 : Colors.black12,
+                    isClicked ? Colors.white70 : Colors.black12,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10)),
                   ),
@@ -72,7 +332,7 @@ class _SearchBoxsState extends State<SearchBoxs> {
                   onPressed: onTap,
                   style: ElevatedButton.styleFrom(
                     backgroundColor:
-                        isClicked ? Colors.black12 : Colors.white70,
+                    isClicked ? Colors.black12 : Colors.white70,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
@@ -110,7 +370,7 @@ class _SearchBoxsState extends State<SearchBoxs> {
                       color: Colors.white,
                     ),
                     contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+                    EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
               ),
             ),
           ),
@@ -134,7 +394,7 @@ class _SearchBoxsState extends State<SearchBoxs> {
                       color: Colors.white,
                     ),
                     contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
+                    EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
               ),
             ),
           ),
@@ -149,13 +409,13 @@ class _SearchBoxsState extends State<SearchBoxs> {
                     color: Colors.black26,
                   ),
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+                  const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
                   child: Row(
                     children: [
                       const Padding(
                         padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
                         child:
-                            Icon(Icons.calendar_month, color: Colors.white70),
+                        Icon(Icons.calendar_month, color: Colors.white70),
                       ),
                       Text(_dateTime.toString(), style: Styles.headLineStyle6)
                     ],
@@ -164,42 +424,41 @@ class _SearchBoxsState extends State<SearchBoxs> {
               ),
             ),
           ),
-          // ListTile(
-          //   title: SizedBox(
-          //     height: MediaQuery.of(context).size.height * 0.06,
-          //     child: InkWell(
-          //       onTap: () {},
-          //       child: Container(
-          //         decoration: BoxDecoration(
-          //           borderRadius: BorderRadius.circular(5),
-          //           color: Colors.black26,
-          //         ),
-          //         padding: const EdgeInsets.symmetric(
-          //           horizontal: 12,
-          //           vertical: 10,
-          //         ),
-          //         child: Row(
-          //           children: const [
-          //             Padding(
-          //               padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
-          //               child: Icon(
-          //                 Icons.supervisor_account,
-          //                 color: Colors.white70,
-          //               ),
-          //             ),
-          //             Text(
-          //               'Traveller',
-          //               style: TextStyle(
-          //                 color: Colors.white,
-          //               ),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          MyListTileWithDropdown(),
+          ListTile(
+            title: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.06,
+              child: InkWell(
+                onTap: () {},
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    color: Colors.black26,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 10,
+                  ),
+                  child: Row(
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                        child: Icon(
+                          Icons.supervisor_account,
+                          color: Colors.white70,
+                        ),
+                      ),
+                      Text(
+                        'Traveller',
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
           const SizedBox(height: 5),
           Center(
             child: Container(
