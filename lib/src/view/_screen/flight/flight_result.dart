0caf848373721847462cf31a53d4c3d41cb81345
omiehsan <74821731+omiehsan@users.noreply.github.com>
2023-08-05 04:51:08 +0600

    Search Update
    Huge progress, App is basically done

--- lib/src/view/_screen/flight/flight_result.dart
+++ lib/src/view/_screen/flight/flight_result.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../../../controller/authentication/models/flights_model.dart'; // Import the FlightSearchModel
 
 class Flight_Result extends StatelessWidget {
   final List<FlightSearchModel> searchResults;
 
   Flight_Result({required this.searchResults});
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -18,14 +17,75 @@ class Flight_Result extends StatelessWidget {
         itemCount: searchResults.length,
         itemBuilder: (context, index) {
           FlightSearchModel flight = searchResults[index];
-          // You can now display the flight details using the `flight` object.
-          return ListTile(
-            title: Text('Airline: ${flight.airline}'),
-            subtitle: Text('Price: ${flight.price}'),
+          final formattedDate = DateFormat('dd/MM/yy').format(flight.date);
 
-            // Display other flight details here as needed.
+          return Card(
+            elevation: 2, // Adjust the elevation for the card as needed
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Airline:'),
+                      Text('${flight.airline}'),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Price:'),
+                      Text('\$${flight.price}'),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('From Destination:'),
+                      Text('${flight.fromDestination}'),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Date:'),
+                      Text('$formattedDate'), // Use the formatted date
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('To Destination:'),
+                      Text('${flight.toDestination}'),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Duration:'),
+                      Text('${flight.duration}'),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Flight Class:'),
+                      Text('${flight.flightClass}'),
+                    ],
+                  ),
+                  // Display other flight details here as needed.
+                ],
+              ),
+            ),
           );
-
         },
       ),
     );
--- lib/src/view/navigations/search_bar/search_boxs.dart
+++ lib/src/view/navigations/search_bar/search_boxs.dart
@@ -1,89 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:hawai_jubu/src/view/_screen/flight/flight.dart';
+import '../../../controller/authentication/controllers/search_controller.dart';
 import 'package:hawai_jubu/src/view/_screen/flight/flight_result.dart';
-import 'package:hawai_jubu/src/view/navigations/search_bar/dropdown.dart';
-import '../../../controller/authentication/models/flights_model.dart';
-import '../../../utils/constaints/styles_colors.dart';
 
-class SearchBoxs extends StatefulWidget {
-  const SearchBoxs({Key? key}) : super(key: key);
-
-  @override
-  _SearchBoxsState createState() => _SearchBoxsState();
-}
-
-class _SearchBoxsState extends State<SearchBoxs> {
-  bool isClicked = false;
-
-  void onTap() {
-    setState(() {
-      isClicked = !isClicked;
-    });
-  }
-
-  DateTime _dateTime = DateTime.now();
-
-  void _showDatePicker() {
-    showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime.now(),
-      lastDate: DateTime(2025),
-    ).then((value) {
-      setState(() {
-        _dateTime = value!;
-      });
-    });
-  }
-
-  void onSearchPressed() async {
-    String fromDestination = "Dhaka"; // Get the value from the From text field.
-    String toDestination = "Chittagong";   // Get the value from the To text field.
-    // DateTime date = _dateTime;
-
-    try {
-      final querySnapshot = await FirebaseFirestore.instance
-          .collection('Destination')
-          .where('From', isEqualTo: fromDestination)
-          .where('To', isEqualTo: toDestination)
-          // .where('Date', isEqualTo: date)
-          .get();
-
-      print('Query executed successfully!');
-      print('Number of results: ${querySnapshot.docs.length}');
-
-
-      final List<FlightSearchModel> flightResults = querySnapshot.docs
-          .map((doc) => FlightSearchModel.fromSnapshot(doc))
-          .toList();
-
-      // Navigate to the FlightResult screen with the search results.
-      Get.to(
-            () => Flight_Result(searchResults: flightResults),
-      );
-
-      for (var flight in flightResults) {
-        print('Airline: ${flight.airline}');
-        print('Price: ${flight.price}');
-        // Display other flight details here as needed.
-        print('---');
-      }
-
-      // Process the query results
-
-      // final List<DocumentSnapshot> flightDocs = querySnapshot.docs;
-
-      // You can now use the data from flightDocs to display the search results
-    } catch (e) {
-      print('Error while searching flights: $e');
-      print('Query executed Unsuccessfully!');
-      // Handle the error if needed
-    }
-  }
+class SearchBoxs extends StatelessWidget {
+  final SearchBoxController controller = Get.put(SearchBoxController());
 
   @override
   Widget build(BuildContext context) {
@@ -94,128 +15,56 @@ class _SearchBoxsState extends State<SearchBoxs> {
         shrinkWrap: true,
         physics: const NeverScrollableScrollPhysics(),
         children: [
-          // Container(
-          //   padding: const EdgeInsets.symmetric(horizontal: 15),
-          //   child: Row(
-          //     children: [
-          //       ElevatedButton(
-          //         onPressed: onTap,
-          //         style: ElevatedButton.styleFrom(
-          //           backgroundColor:
-          //               isClicked ? Colors.white70 : Colors.black12,
-          //           shape: RoundedRectangleBorder(
-          //               borderRadius: BorderRadius.circular(10)),
-          //         ),
-          //         child: const Text(
-          //           ' One Way ',
-          //           style: TextStyle(
-          //             fontSize: 16,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.white,
-          //           ),
-          //         ),
-          //       ),
-          //       const SizedBox(
-          //         width: 8,
-          //       ),
-          //       ElevatedButton(
-          //         onPressed: onTap,
-          //         style: ElevatedButton.styleFrom(
-          //           backgroundColor:
-          //               isClicked ? Colors.black12 : Colors.white70,
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(10),
-          //           ),
-          //         ),
-          //         child: const Text(
-          //           'Round Trip',
-          //           style: TextStyle(
-          //             fontSize: 16,
-          //             fontWeight: FontWeight.bold,
-          //             color: Colors.black,
-          //           ),
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          const SizedBox(height: 5),
           ListTile(
             title: SizedBox(
               height: MediaQuery.of(context).size.height * 0.06,
               child: TextFormField(
+                onChanged: (value) => controller.setFromDestination(value),
                 decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide.none,
-                    ),
-                    fillColor: Colors.black26,
-                    filled: true,
-                    prefixIcon: const Icon(
-                      Icons.location_on,
-                      color: Colors.white70,
-                    ),
-                    hintText: 'From',
-                    hintStyle: const TextStyle(
-                      color: Colors.white,
-                    ),
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
-              ),
-            ),
-          ),
-          ListTile(
-            title: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
-              child: TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                      borderSide: BorderSide.none,
-                    ),
-                    fillColor: Colors.black26,
-                    filled: true,
-                    prefixIcon: const Icon(
-                      FluentSystemIcons.ic_fluent_globe_location_regular,
-                      color: Colors.white70,
-                    ),
-                    hintText: 'To',
-                    hintStyle: TextStyle(
-                      color: Colors.white,
-                    ),
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 10, vertical: 10)),
-              ),
-            ),
-          ),
-          ListTile(
-            title: SizedBox(
-              height: MediaQuery.of(context).size.height * 0.06,
-              child: InkWell(
-                onTap: _showDatePicker,
-                child: Container(
-                  decoration: BoxDecoration(
+                  border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(5),
-                    color: Colors.black26,
+                    borderSide: BorderSide.none,
                   ),
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
-                  child: Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
-                        child:
-                            Icon(Icons.calendar_month, color: Colors.white70),
-                      ),
-                      Text(_dateTime.toString(), style: Styles.headLineStyle6)
-                    ],
+                  fillColor: Colors.black26,
+                  filled: true,
+                  prefixIcon: const Icon(
+                    Icons.location_on,
+                    color: Colors.white70,
                   ),
+                  hintText: 'From',
+                  hintStyle: const TextStyle(
+                    color: Colors.white,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
                 ),
               ),
             ),
           ),
-          MyListTileWithDropdown(),
-          const SizedBox(height: 5),
+          ListTile(
+            title: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.06,
+              child: TextFormField(
+                onChanged: (value) => controller.setToDestination(value),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5),
+                    borderSide: BorderSide.none,
+                  ),
+                  fillColor: Colors.black26,
+                  filled: true,
+                  prefixIcon: const Icon(
+                    Icons.flight_takeoff,
+                    color: Colors.white70,
+                  ),
+                  hintText: 'To',
+                  hintStyle: TextStyle(
+                    color: Colors.white,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                ),
+              ),
+            ),
+          ),
           Center(
             child: Container(
               width: MediaQuery.of(context).size.width * 0.9,
@@ -227,8 +76,13 @@ class _SearchBoxsState extends State<SearchBoxs> {
                 ),
               ),
               child: ElevatedButton(
-                onPressed: () => onSearchPressed(),
-                    // Get.to(() =>  Flight_Result(searchResults: [],)),
+                onPressed: () async {
+                  await controller.onSearchPressed();
+                  // Once the search is completed, navigate to the Flight_Result page.
+                  if (controller.searchResults.isNotEmpty) {
+                    Get.to(() => Flight_Result(searchResults: controller.searchResults));
+                  }
+                },
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.transparent,
                   shape: RoundedRectangleBorder(
