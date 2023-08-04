f7cd0902f256de20ab3b798029122d1cafa3e1f6
omiehsan <74821731+omiehsan@users.noreply.github.com>
2023-08-05 05:12:02 +0600

    Destination details

--- lib/src/view/_screen/flight/flight_result.dart
+++ lib/src/view/_screen/flight/flight_result.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../../../controller/authentication/models/flights_model.dart'; // Import the FlightSearchModel
+import '../../flights/destination_details.dart';
 
 class Flight_Result extends StatelessWidget {
   final List<FlightSearchModel> searchResults;
@@ -19,70 +20,81 @@ class Flight_Result extends StatelessWidget {
           FlightSearchModel flight = searchResults[index];
           final formattedDate = DateFormat('dd/MM/yy').format(flight.date);
 
-          return Card(
-            elevation: 2, // Adjust the elevation for the card as needed
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Airline:'),
-                      Text('${flight.airline}'),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Price:'),
-                      Text('\$${flight.price}'),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('From Destination:'),
-                      Text('${flight.fromDestination}'),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Date:'),
-                      Text('$formattedDate'), // Use the formatted date
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('To Destination:'),
-                      Text('${flight.toDestination}'),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Duration:'),
-                      Text('${flight.duration}'),
-                    ],
-                  ),
-                  SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text('Flight Class:'),
-                      Text('${flight.flightClass}'),
-                    ],
-                  ),
-                  // Display other flight details here as needed.
-                ],
+          return GestureDetector(
+            onTap: () {
+              // Navigate to the DestinationDetailsPage with the selected flight data.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DestinationDetailsPage(flight: flight),
+                ),
+              );
+            },
+            child: Card(
+              elevation: 2,
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Airline:'),
+                        Text('${flight.airline}'),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Price:'),
+                        Text('\$${flight.price}'),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('From Destination:'),
+                        Text('${flight.fromDestination}'),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Date:'),
+                        Text('$formattedDate'), // Use the formatted date
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('To Destination:'),
+                        Text('${flight.toDestination}'),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Duration:'),
+                        Text('${flight.duration}'),
+                      ],
+                    ),
+                    SizedBox(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text('Flight Class:'),
+                        Text('${flight.flightClass}'),
+                      ],
+                    ),
+                    // Display other flight details here as needed.
+                  ],
+                ),
               ),
             ),
           );
